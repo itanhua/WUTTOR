bec171386f58b5004bbf2eac75929848a00ec6c3
TANHUA <kmo@live.com>
2026-09-08 11:01:01 +0800

    Reset site configuration values
    Cleared configuration values for title, subtitle, description, installed_at, keywords, and favicon.

--- config/site.php
+++ config/site.php
@@ -1,11 +1,11 @@
 <?php
 return array (
-  'title' => 'Ai BBS',
-  'subtitle' => '一个自由的Ai交流社区',
-  'description' => 'AiBBS 是一个专注于内容分享与社区交流的轻量级论坛系统，致力打造简洁、易用、温暖的讨论空间。',
-  'installed_at' => '2026-08-21 15:29:58',
+  'title' => ' ',
+  'subtitle' => ' ',
+  'description' => ' ',
+  'installed_at' => ' ',
   'version' => '1.0.0',
   'logo' => '',
-  'keywords' => 'ai论坛,大模型语言,aibbs,ai工具,ai,多模态,aigc,ai开源,机器人学习,人工智能,深度学习,LLM',
-  'favicon' => 'uploads/post/202608/20260824_104524_f1935a2597d3.png',
+  'keywords' => ' ',
+  'favicon' => ' ',
 );
